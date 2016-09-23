77ac9dd8fb6a32d415f4448fd4c2fbfc23e7126e
Pierre BOUDEAUD <pierre.boudeaud@live.fr>
2016-09-23 08:06:57 +0200

    Ajout classe Utilisateur

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 	require("/php/Utilisateur.php");
-	$mdp = sha1("dui480");
-	$util = new Utilisateur(1, "deltadu17", "pierre.boudeaud@live.fr", $mdp);
+	$util = new Utilisateur(3, "deltadu17", "pierre.boudeaud@live.fr", "dui480");
 	$str = "Utilisateur : ".$util->getId()." ".$util->getPseudo()." ".$util->getEMail()." ".$util->getMDP();
 	echo $str;
 	$util->create();
+	$rep = $util->read();
+	print_r($rep->fetchAll());
+	$util->update($util->getId(), "Jojo Bernard", "jojo.bernard@trololol.com", "123");
+	$util->setPseudo("Jojo Bernard");
+	$util->setEMail("jojo.bernard@trololol.com");
+	$util->setMDP("123");
+	$str = "Utilisateur : ".$util->getId()." ".$util->getPseudo()." ".$util->getEMail()." ".$util->getMDP();
+	echo $str;
+	$util->delete($util->getId());
 ?>
--- php/Utilisateur.php
+++ php/Utilisateur.php
@@ -25,30 +25,61 @@
 			return $this->mDP;
 		}
 
+		public function setId($id){
+			$this->id = $id;
+		}
+
+		public function setPseudo($pseudo){
+			$this->pseudo = $pseudo;
+		}
+
+		public function setEMail($eMail){
+			$this->eMail = $eMail;
+		}
+
+		public function setmDP($mDP){
+			$this->mDP = $mDP;
+		}
+
 		public function create(){
-			$bdd = connexion();
-			$req = "INSERT INTO UTILISATEUR VALUES(" . $this->id . ", " . $this->pseudo . ", " . $this->eMail . ", " . $this->mDP . ")";
+			$bdd = $this->connexion();
+			$req = "INSERT INTO UTILISATEUR(pseudoutilisateur, emailutilisateur, mdputilisateur) VALUES('" . $this->pseudo . "', '" . $this->eMail . "', '" . $this->mDP . "')";
 			echo "<br>".$req."<br>";
-			//$bdd->prepare($req);
+			$bdd->exec($req);
 		}
 
-		function connexion(){
-		$ModeBdd = "pgsql"; //pgsql : Postgres | mysql : MySQL
-		$Adresse = "localhost";
-		$Pseudo = "postgres";
-		$MDP = "pgadmin";
-		$dbName = "Forum";
-
-		$dsn = $ModeBdd.":dbname=".$dbName.";host=".$Adresse;
-		echo "<br>$dsn<br>";
-		try{
-			$DB = new PDO($dsn, $Pseudo, $MDP);
-		}catch(PDOException $e){
-			echo "Connexion échouée : ".$e->getMessage();
-			$DB = null;
+		public function read(){
+			$bdd = $this->connexion();
+			$req = "SELECT * FROM UTILISATEUR";
+			$rep = $bdd->query($req);
+			return $rep;
 		}
 
-		return($DB);
-	}
+		public function update($idUtilisateur, $newPseudo, $newEmailUtilisateur, $newMdpUtilisateur){
+			$bdd = $this->connexion();
+			$req = "UPDATE UTILISATEUR SET pseudoutilisateur = '$newPseudo', emailutilisateur = '$newEmailUtilisateur', mdputilisateur = '$newMdpUtilisateur' WHERE idUtilisateur = $idUtilisateur";
+			echo $req;
+			$bdd->exec($req);
+		}
+
+		public function delete($idUtilisateur){
+			$bdd = $this->connexion();
+			$req = "DELETE FROM UTILISATEUR WHERE idUtilisateur = $idUtilisateur";
+			echo $req;
+			$bdd->exec($req);
+		}
+
+		private function connexion(){
+			$ini_parse = parse_ini_file("cfg/bdd.ini");
+			$dsn = $ini_parse['type'].":dbname=".$ini_parse['dbName'].";host=".$ini_parse['host'].";port=".$ini_parse['port'];
+			echo "<br>$dsn<br>";
+			try{
+				$DB = new PDO($dsn, $ini_parse['pseudo'], $ini_parse['mdp']);
+			}catch(PDOException $e){
+				echo "Connexion échouée : ".$e->getMessage();
+				$DB = null;
+			}
+			return $DB;
+		}
 	}
 ?>
